80f2ac9f399759987ea230dfb0bacb3af930d803
Karl Casserfelt <karl.casserfelt@gmail.com>
2020-10-30 20:32:46 +0100

    change keybinding for focusmaster

--- config.def.h
+++ config.def.h
@@ -99,7 +99,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },
+	{ MODKEY|ControlMask,           XK_Return, focusmaster,    {0} },
 };
 
 /* button definitions */
