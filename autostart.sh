161e74ef7173a47282f3f5032bce1c2f4707693f
Karl Casserfelt <karl.casserfelt@axis.com>
2021-05-03 17:22:11 +0200

    add xmodmap and xcape to autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 nitrogen --restore &
-xbindkeys &
+xbindkeys || true & 
 xset r rate 350 32 &
 while true; do xsetroot -name "$( date +"%F %R" ) $(cat /sys/class/power_supply/BAT0/capacity)%"; sleep 10s; done &
+xmodmap ~/.xmodmap &
+xcape -e 'Mode_switch=Escape'
+
